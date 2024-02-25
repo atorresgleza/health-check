8624b373a95dffc93626ae0f2355a5122ad8084f
devkappa <atorresgleza@gmail.com>
2024-02-25 00:17:31 -0800

    fit: curl url and test status code

---
+++ health-check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+website=$1
+ok=0
+status=(502 503 500 304 405 429 404)
+
+is_ok() {
+  echo "OK"
+}
+
+for code in "${status[@]}"; do
+  if [ "$(curl -I -s $website | awk '/^HTTP/{print $2}')" = $code ]; then
+    echo "BAD"
+    ok=1 
+    break
+  fi
+done
+
+if [ "$ok" -eq 0 ]; then
+  is_ok
+fi
