4461491f3790f4b1ca33a1eca4cd150d620b1080
devkappa <atorresgleza@gmail.com>
2024-03-10 23:30:36 -0700

    fix(script): improve HTTP status code handling in health-check.sh
    - Add variable to store response message for clearer output.
    - Use a single call to `curl` to fetch HTTP status code and message, reducing the number of requests.
    - Introduce `is_ok` function to output the result of the health check, enhancing readability.
    - Ensure the script exits with the appropriate status by setting the `ok` variable based on the HTTP status code.

--- health-check.sh
+++ health-check.sh
@@ -2,20 +2,28 @@
 
 website=$1
 ok=0
+res=""
 status=(304 404 405 429 500 502 503)
 
+read HTTP CODE MSG <<< "$(curl -I -s $website | awk '/^HTTP/{print $1, $2, $3}')"
+
 is_ok() {
+  if [ "$ok" -eq 0 ]; then 
+    echo "[+] OK -> $HTTP $CODE $MSG" 
+    else echo "[-] FAIL -> $HTTP $CODE $MSG" 
+  fi
   exit $ok
 }
 
 for code in "${status[@]}"; do
-  if [ "$(curl -I -s $website | awk '/^HTTP/{print $2}')" = $code ]; then    
-    ok=1 
+  if [ $CODE = $code ]; then    
+    ok=1
     is_ok
     break
   fi
 done
 
 if [ "$ok" -eq 0 ]; then
+  res=$CODE
   is_ok
 fi
