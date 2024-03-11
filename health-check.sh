a386ec93e4ce26019deae8ee8a1f25077b4d976f
devkappa <atorresgleza@gmail.com>
2024-03-11 02:17:23 -0700

    refactor(health-check): Improve status code handling and output messages
    This commit enhances the script's structure and readability by refactoring functions and variable names for clarity. It introduces separate functions for message display (msg) and status code evaluation (search_bad), along with a refined logic flow that accurately handles HTTP status codes and curl exit statuses. The update aims to improve maintainability and extend the script's functionality for future adjustments.
    
    - Rename ok to curl_exit_status to clearly represent the curl command's exit status.
    - Change status array to status_bad for explicit naming of undesired HTTP status codes.
    - Replace is_ok with more descriptive function names (msg for messaging and search_bad for status code evaluation).
    - Introduce error handling for missing HTTP status code and curl command failure.
    - Simplify logic flow in is_ok to directly assess and respond to the HTTP status code and curl exit status.

--- health-check.sh
+++ health-check.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
 website=$1
-ok=0
 res=""
-status=(304 404 405 429 500 502 503)
+status_bad=(304 404 405 429 500 502 503)
 
 read HTTP CODE MSG <<< "$(curl -I -s $website | awk '/^HTTP/{print $1, $2, $3}')"
+curl_exit_status=$?
 
-is_ok() {
-  if [ "$ok" -eq 0 ]; then 
-    echo "[+] OK -> $HTTP $CODE $MSG" 
-    else echo "[-] FAIL -> $HTTP $CODE $MSG" 
-  fi
-  exit $ok
+msg() {
+  echo "$1 $HTTP $CODE $MSG"
+  exit $2
 }
 
-for code in "${status[@]}"; do
-  if [ "$CODE" = "$code" ]; then    
-    ok=1
-    is_ok
-    break
-  fi
-done
+searh_bad() {
+  local st=$1
+  for code in "${status_bad[@]}"; do
+    if [ "$st" = "$code" ]; then    
+      return 1
+      break
+    fi
+  done
+  return 0
+}
 
-if [ "$ok" -eq 0 ]; then
-  res=$CODE
-  is_ok
-fi
+is_ok() {
+  local st=$1
+  local code=$2
+  if [ -z "$code" ]; then 
+       msg "[-] FAIL" 1; 
+  else searh_bad "$code"
+    if [ $? -eq 0 ]; then
+       msg "[+] OK" 0
+    else msg "[-] FAIL" 1
+    fi 
+  fi
+}
+
+is_ok $curl_exit_status $CODE
