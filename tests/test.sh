3e86277fee9318b9d8944e9dd7214760307e55a5
devkappa <atorresgleza@gmail.com>
2024-03-25 23:40:52 -0700

    test: add test for successful website response

--- tests/test.sh
+++ tests/test.sh
@@ -5,3 +5,9 @@ function set_up() {
   SCRIPT="$ROOT_DIR/health-check.sh"
   MURL="http://example.com"
 }
+
+function testOKWebsiteResponds() {
+    mock curl echo "HTTP/1.1 200 OK"
+    assert_equals "[+] OK HTTP/1.1 200 OK" "$( $SCRIPT "$MURL" )"
+}
+
